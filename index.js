9ae91deefe5edf1bec076c29ce73a0e0e41e8b7a
vikas kumar <vikas10145.12@bitmesra.ac.in>
2024-11-15 17:30:05 +0530

    Checkbox functionality and css
    -Checkbox functionality and css using Javascript

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function myFunction() {
     }
   }
 
-  //Functoin for checkbox to show bottom box
+  //Functoin for checkbox to show bottom box only one at a time out of three boxes
 
   function myFunctionCheck() {
     var checkBox1 = document.getElementById("myCheck1");
@@ -28,18 +28,45 @@ function myFunction() {
     var box1 = document.getElementById("bottom-box1");
     var box2 = document.getElementById("bottom-box2");
     var box3 = document.getElementById("bottom-box3");
-    if (checkBox1.checked == true){
+    if (checkBox1.checked == true && checkBox2.checked == false && checkBox3.checked == false){
       box1.style.display = "block";
       box2.style.display = "none";
       box3.style.display = "none";
-    } else if(checkBox2.checked == true){
+      checkBox2.checked = false;
+      checkBox3.checked = false;
+      var  innerBox2 = document.getElementById("inner-container-box2")
+      innerBox2.style.border = "2px solid #FF6B82";
+      innerBox2.style.margin = "0 0 25px 0";
+      document.getElementById("inner-top-box2").style.border = "none";
+
+    } else if(checkBox1.checked == false && checkBox2.checked == true && checkBox3.checked == false){
       box1.style.display = "none";
       box2.style.display = "block";
       box3.style.display = "none";
+      checkBox1.checked = false;
+      checkBox3.checked = false;
+      var  innerBox3 = document.getElementById("inner-container-box3")
+      innerBox3.style.border = "2px solid #FF6B82";
+      innerBox3.style.margin = "0 0 25px 0";
+      document.getElementById("inner-top-box3").style.border = "none";
     }
-    else if(checkBox3.checked == true) {
+    else if(checkBox1.checked == false && checkBox2.checked == false && checkBox3.checked == true) {
       box1.style.display = "none";
       box2.style.display = "none";
       box3.style.display = "block";
+      checkBox1.checked = false;
+      checkBox2.checked = false;
+      var  innerBox4 = document.getElementById("inner-container-box4")
+      innerBox4.style.border = "2px solid #FF6B82";
+      innerBox4.style.margin = "0 0 25px 0";
+      document.getElementById("inner-top-box4").style.border = "none";
+    }
+    else {
+      box1.style.display = "none";
+      box2.style.display = "none";
+      box3.style.display = "none";
+      checkBox1.checked = false;
+      checkBox2.checked = false;
+      checkBox3.checked = false;
     }
   }
