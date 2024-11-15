543103de2b01b09f131a08bc2a1ff3cb8799c1e3
vikas kumar <vikas10145.12@bitmesra.ac.in>
2024-11-15 16:41:17 +0530

    checkbox functionality
    -checkbox functionality

--- index.js
+++ index.js
@@ -18,3 +18,28 @@ function myFunction() {
       }
     }
   }
+
+  //Functoin for checkbox to show bottom box
+
+  function myFunctionCheck() {
+    var checkBox1 = document.getElementById("myCheck1");
+    var checkBox2 = document.getElementById("myCheck2");
+    var checkBox3 = document.getElementById("myCheck3");
+    var box1 = document.getElementById("bottom-box1");
+    var box2 = document.getElementById("bottom-box2");
+    var box3 = document.getElementById("bottom-box3");
+    if (checkBox1.checked == true){
+      box1.style.display = "block";
+      box2.style.display = "none";
+      box3.style.display = "none";
+    } else if(checkBox2.checked == true){
+      box1.style.display = "none";
+      box2.style.display = "block";
+      box3.style.display = "none";
+    }
+    else if(checkBox3.checked == true) {
+      box1.style.display = "none";
+      box2.style.display = "none";
+      box3.style.display = "block";
+    }
+  }
